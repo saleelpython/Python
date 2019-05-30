5d4a24fce8580b8274f6480bbab6efdd6e24251a
saleel <saleel.python@gmail.com>
2019-05-30 10:52:58 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("Hello Wrold")
+def fn(firstName, lastName, canVote=True, canDrive=True):
+    print(firstName, end=" ")
+    print(lastName)
+
+fn('Saleel', 'Bagde')
+fn(lastName='Patil', firstName='Nitish')
