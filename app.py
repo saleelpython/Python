907c492efb387643c9e58eef43168a5091519844
saleel <saleel.python@gmail.com>
2019-05-30 11:51:32 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 def fn(firstName, lastName, canVote=True, canDrive=True):
     print(firstName, end=" ")
     print(lastName)
+    print("can vote", canVote, end=" ")
+    print("can drive", canDrive)
 
 fn('Saleel', 'Bagde')
+
+print("---------------------------")
 fn(lastName='Patil', firstName='Nitish')
+
+
+print("---------------------------")
+fn('Sharmin', lastName='Bagde')
