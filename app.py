283dbb77cc875728a9248dd330871dbc26e7a54d
saleel <saleel.github@gmail.com>
2019-06-03 09:07:51 +0530

    First commit

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-import module1
+# def char_frequency(str1):
+#     dict = {}
+#     for n in str1:
+#         keys = dict.keys()
+#         if n in keys:
+#             dict[n] += 1
+#         else:
+#             dict[n] = 1
+#     return dict
+# print(char_frequency('saleel'))
 
-print(module1.person)
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday']
+
+for d in days:
+    print(days.count(d))
 
 
--- module1.py
+++ module1.py
@@ -1,6 +1,32 @@
-person = [{"firstName": "Saleel", "lastName": "Bagde"}]
+# person = [{"firstName": "Saleel", "lastName": "Bagde"}]
 
-def printPerson(per):
-    for key, value in per.items():
-        print(f"{key}{value}")
+# def printPerson(per):
+#     for key, value in per.items():
+#         print(f"{key}{value}")
     
+
+# codeNumber = [1, 56, 7, 4, 2, 4, 7, 5, 34, 67, 42, 2, 4, 5,]
+
+
+
+# def getCodeNumber(c):
+#         if c > 10:
+#                 return True
+#         else:
+#                 return False
+
+
+# import module1
+
+# # # print(module1.person)
+
+# # for x in module1.codeNumber:
+# #     if x > 10:
+# #         print(f"item code [{x}] cannot be processed")
+# #         continue
+# #     print(f"processed items {x}")
+
+# # c = list(filter(module1.getCodeNumber, module1.codeNumber))
+
+
+# # print(c)
