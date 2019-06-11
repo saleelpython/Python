f0e1520a31a1da8a219517b72e8b1f3e3719bf4e
saleel <saleel.python@gmail.com>
2019-06-11 07:17:52 +0530

    Commit

--- module1.py
+++ module1.py
@@ -58,3 +58,47 @@
 
 # for x in range(1, 1000):
 #     print(round(random.random(),5))
+
+
+# ll = {}
+
+# for i in l:
+#    ll[i] = abs(i-24)
+# #    print(i, abs(i-15))
+
+# m = min(ll.values())
+# print(f"----------------Key: {m}------------------")
+# print()
+# for key, value in ll.items():
+#     # print(f"Key->{key}  value->{value}")
+#     if value == m:
+#         print(key)
+#         break
+
+
+
+# l1 = ['sharmin', 'ruhan', 'nitish', 'saleel', 'neel', 'deep', 'gau', 'saleel']
+# l2 = ('sharmin', 'ruhan', 'nitish', 'saleel', 'neel', 'deep', 'gau', 'saleel')
+
+# for key, value in enumerate(l1,100):
+#    print(f"{key} - {value}")
+# print("------------------------------")
+# for key, value in enumerate(l2):
+#    print(f"{key} - {value}")
+
+# def fn(**kwargs):
+#    for keys, values in kwargs.items():
+#       print(f"{keys} {values}")
+
+# fn(firstName='Saleel', lastName='Bagde')
+
+# colours = (
+#     ('Yasoob', 'Yellow'),
+#     ('Ali', 'Blue'),
+#     ('Arham', 'Green'),
+#     ('Ali', 'Black'),
+#     ('Yasoob', 'Red'),
+#     ('Ahmed', 'Silver'),
+# )
+
+# print(colours)
