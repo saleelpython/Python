e02e232d2a72a4f70305e8ea513ce467833f7803
saleel <saleel.python@gmail.com>
2019-06-12 13:09:40 +0530

    Commit First

--- Demo.py
+++ Demo.py
@@ -102,3 +102,37 @@
 # )
 
 # print(colours)
+
+
+
+
+import json
+
+with open('Superstore.json') as p:
+    data = json.load(p)
+
+# for i in range(0, len(data)):
+#     print(f"Document {i}")
+#     print();
+#     for key, value in data[i].items():
+#         print(f"{key} : {value}")
+#     print();
+#     print("-----------------------------------")
+
+
+# def getRegion(d):
+#     if d['Region'] in "West":
+#         return True
+#     else:
+#         return False
+
+# r = list(filter(getRegion, data))
+
+# for i in range(0, len(r)):
+#     print(f"Document {i}")
+#     print(r[i]['Row ID'])
+#     print(r[i]['Order ID'])
+#     print(r[i]['Order Date'])
+#     print((r[i]['Region']))
+
+#     print("-----------------------------")
