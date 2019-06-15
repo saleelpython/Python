ac8ff4e8d54fed7aa269eb44e9c7c817f92b451b
saleel <saleel.python@gmail.com>
2019-06-15 15:20:50 +0530

    commit Second

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 import tkinter as tk
-# import tkMessageBox as tkm
 
 win = tk.Tk()
-win.title("Infoway Technologies pvt., ltd")
+win.geometry("800x100")
+Title = tk.StringVar()
+Title.set("Infoway Technologies pvt., ltd by saleel from Python")
+win.title(Title.get())
 f1 = tk.Frame(win, width=600, height=400, bg='tomato')
-x = 0;
+
+counter = tk.IntVar()
+
+str1 = tk.StringVar()
+str1.set("Output Window...")
 
 def fn(event):
-    print( "Hello Python", "Hello World", x)
+    counter.set(counter.get() + 1)
+    print("Hello Python", "Hello World", counter.get())
+    print(userName.get())
+    
+def fn1(event):
+    print("<Enter>")
 
-userName = tk.Entry(f1)
-userName.pack()
+userName = tk.Entry(f1, width='30')
+userName.grid(row=0, column=0)
+# userName.pack()
 
-btn = tk.Button(f1, text="Click here")
+btn = tk.Button(f1, text="Python", width='30')
 btn.bind("<Button-1>", fn)
-btn.pack()
+btn.bind("<Enter>", fn1)
+btn.grid(row=0, column=1)
+# btn.pack()
+
+lbl1 = tk.Label(f1, text=str1.get(), width='30')
+lbl1.grid(row=0, column=2)
 
 f1.pack(side='left')
 
