6eeed98ce6ae6092fe511639d10c59ca883bb159
saleel <saleel.python@gmail.com>
2019-06-24 07:13:02 +0530

    commit

--- Demo.py
+++ Demo.py
@@ -180,5 +180,44 @@
 # r.mainloop() 
 
 
+# -------------------------------
 
+# import tkinter as tk
+
+# win = tk.Tk()
+# win.geometry("645x100+500+200")
+# Title = tk.StringVar()
+# Title.set("Infoway Technologies pvt., ltd by saleel from Python")
+# win.title(Title.get())
+# f1 = tk.Frame(win, width=600, height=400, bg='tomato')
+
+# counter = tk.IntVar()
+
+# str1 = tk.StringVar()
+# str1.set("Output Window...")
+
+# def fn(event):
+#     counter.set(counter.get() + 1)
+#     print("Hello Python", "Hello World", counter.get())
+#     print(userName.get())
+    
+# def fn1(event):
+#     print("<Enter>")
+
+# userName = tk.Entry(f1, width='30')
+# userName.grid(row=0, column=0)
+# # userName.pack()
+
+# btn = tk.Button(f1, text="Python", width='30')
+# btn.bind("<Button-1>", fn)
+# btn.bind("<Enter>", fn1)
+# btn.grid(row=0, column=1)
+# # btn.pack()
+
+# lbl1 = tk.Label(f1, text=str1.get(), width='30')
+# lbl1.grid(row=0, column=2)
+
+# f1.pack(side='left')
+
+# win.mainloop()
 
