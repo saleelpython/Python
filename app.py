fbbc584f2ab412ab89f163cafa48153e85c4af90
saleel <saleel.python@gmail.com>
2019-07-10 15:41:51 +0530

    commit

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 #!C:\Users\saleel\AppData\Local\Programs\Python\Python37-32\python
+# Comprehensions in Python
 
-person = {"ID": "1001", "firstName": "Saleel", "lastName": "Bagde", "canVote": "True", "canDrive": "True"}
-
-l = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-
-def fn(l):
-    x = iter(l)
-
-    while True:
-        try:
-            print(next(x))
-        except StopIteration as ex:
-            print('-' * 16)
-            print("data not Dound")
-            break
-
-fn(l)
+fruits = ['papaya', 'lime', 'grapes', 'pear', 'orange', 'banana', 'apple', 'orange', 'fig', 'blackberry', 'blueberry', 'mango', 'pineapple', 'kiwi', '', 'cherry', 'lemon', 'dragonfruit', 'watermelon']
 
 
+# l = [x.upper() for x in fruits]
+# print(l)
 
 
+st = "This is the 20A test 535 by 1783 saleel"
 
-
-
+l = [x for x in st if x.isdigit()]
+print(l)
 
         
