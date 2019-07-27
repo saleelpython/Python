85ceed5d8f03e34e4d7458a77d41bc4df0da70e6
saleel <saleel.python@gmail.com>
2019-07-27 17:55:36 +0530

    Commit

--- app.py
+++ app.py
@@ -3,6 +3,26 @@
 
 fruit = ['blackberry', 'grapes', 'blueberry', 'fig', 'lemon', 'watermelon', 'apple', 'kiwi', 'lime', 'mango','blackberry']
 
+country = ["Afghanistan", "Italy", "India", "Japan", "United Kingdom", "Vietnam", "Russia", "Nepal", "Bangladesh"]
 
+capital = ["Kabul", "Rome", "New Delhi", "Tokyo", "London", "Hanoi", "Moscow", "Kathmandu", "Dhaka"]
 
+# cities = [[1,2], [3,4], [5,6], [7,8], [9, 10], [11, 12], [13,14], [15,16], [17,18]]
 
+# for x in cities:
+#     for y in x:
+#         print(y)
+#     print("-" * 20)
+
+y = 1234567
+# print (y % 10)
+# x = round(y / 10)
+# print(x)
+# print(y % 10)
+
+while (y >0):
+    a = (y % 10)
+    y = round(y // 10)
+    print(a)
+    
+    
