7f7484a3da06ff0749e3831ab1b00c3a4041501c
saleel <saleel.python@gmail.com>
2019-07-30 09:29:38 +0530

    Commit

--- Pattern.py
+++ Pattern.py
@@ -1,8 +1,27 @@
+n = 123
+num = []
+first = True
+while (n > 0):
+    if first:
+        num.append(n % 2)
+        n = (n // 2)
+        first=False
+    else:
+        num.append(n % 2)
+        n = (n // 2)
+num.reverse()
+print(num)
+
+
+
+y=1234
 while (y >0):
     a = (y % 10)
-    y = round(y // 10)
+    y = (y // 10)
     print(a)
     
+
+
 
 for x in range(1,9):
     for y in range(x,9):
--- app.py
+++ app.py
@@ -7,18 +7,10 @@ country = ["Afghanistan", "Italy", "India", "Japan", "United Kingdom", "Vietnam"
 
 capital = ["Kabul", "Rome", "New Delhi", "Tokyo", "London", "Hanoi", "Moscow", "Kathmandu", "Dhaka"]
 
-# cities = [[1,2], [3,4], [5,6], [7,8], [9, 10], [11, 12], [13,14], [15,16], [17,18]]
-
-# for x in cities:
-#     for y in x:
-#         print(y)
-#     print("-" * 20)
-
-y = 1234567
-# print (y % 10)
-# x = round(y / 10)
-# print(x)
-# print(y % 10)
+cities = [[1,2], [3,4], [5,6], [7,8], [9, 10], [11, 12], [13,14], [15,16], [17,18]]
 
 
+
+   
+    
     
