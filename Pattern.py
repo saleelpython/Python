d180a506f2aef0cd172b1f5442ac057d660b72af
unknown <saleel@stp5.infoway.com>
2019-08-19 11:03:39 +0530

    Commit

--- Pattern.py
+++ Pattern.py
@@ -27,3 +27,20 @@ for x in range(1,9):
     for y in range(x,9):
         print(y, end="")
     print()
+
+
+
+n = int(input("Enter number "))
+num = []
+first = True
+while (n > 0):
+    if first:
+        num.append(n % 2)
+        n = (n // 2)
+        first=False
+    else:
+        num.append(n % 2)
+        n = (n // 2)
+num.reverse()
+print(num)
+   
--- app.py
+++ app.py
@@ -10,7 +10,11 @@ capital = ["Kabul", "Rome", "New Delhi", "Tokyo", "London", "Hanoi", "Moscow", "
 cities = [[1,2], [3,4], [5,6], [7,8], [9, 10], [11, 12], [13,14], [15,16], [17,18]]
 
 
+a = 5
+b = 1
+for x in range(1, a + 1):
+    b *= x
+    print(x)
+print(b)
 
-   
-    
-    
+1 + 2 + 3 + 4 + 5 + 6
