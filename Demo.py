73ecb9bfe25e37f74fc4b3f463735f8dc49c36c1
SaleelBagde <saleel.python@gmail.com>
2019-08-24 14:47:29 +0530

    Commited on 2019-08-23

--- Demo.py
+++ Demo.py
@@ -1,6 +1,35 @@
 # True and non-empty things are true. Number 1 is true.
 # False, None, zero, and empty things are false.
 
+#!C:\Users\saleel\AppData\Local\Programs\Python\Python37-32\python
+# Comprehensions in Python
+
+
+# fruit = ['blackberry', 'grapes', 'blueberry', 'fig', 'lemon', 'watermelon', 'apple', 'kiwi', 'lime', 'mango','blackberry']
+
+# country = ["Afghanistan", "Italy", "India", "Japan", "United Kingdom", "Vietnam", "Russia", "Nepal", "Bangladesh"]
+
+# capital = ["Kabul", "Rome", "New Delhi", "Tokyo", "London", "Hanoi", "Moscow", "Kathmandu", "Dhaka"]
+
+# cities = [[1,2], [3,4], [5,6], [7,8], [9, 10], [11, 12], [13,14], [15,16], [17,18]]
+
+
+# a = 5
+# b = 1
+# for x in range(1, a + 1):
+#     b *= x
+#     print(x)
+# print(b)
+
+
+# www ="www.google.com"
+# d = dict()
+# for x in www:
+#    d[x] = www.count(x)
+
+# print(d)
+
+
 
 # person = [{"firstName": "Saleel", "lastName": "Bagde"}]
 
--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-#!C:\Users\saleel\AppData\Local\Programs\Python\Python37-32\python
-# Comprehensions in Python
+www ="www.google.com"
+d = dict()
+for x in www:
+   d[x] = www.count(x)
 
-fruit = ['blackberry', 'grapes', 'blueberry', 'fig', 'lemon', 'watermelon', 'apple', 'kiwi', 'lime', 'mango','blackberry']
-
-country = ["Afghanistan", "Italy", "India", "Japan", "United Kingdom", "Vietnam", "Russia", "Nepal", "Bangladesh"]
-
-capital = ["Kabul", "Rome", "New Delhi", "Tokyo", "London", "Hanoi", "Moscow", "Kathmandu", "Dhaka"]
-
-cities = [[1,2], [3,4], [5,6], [7,8], [9, 10], [11, 12], [13,14], [15,16], [17,18]]
-
-
-a = 5
-b = 1
-for x in range(1, a + 1):
-    b *= x
-    print(x)
-print(b)
-
+print(d)
