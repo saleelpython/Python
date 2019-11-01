333828a63a1bbccfa711af6e514dc0b2d47c2e50
SaleelBagde <saleel.python@gmail.com>
2019-11-01 15:41:22 +0530

    Commited on 01-11-2019

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-l = [i for i in range(1,11) if i < 7]
+time = int(input("Enter Time "))
 
-for key, value in enumerate(l, start=100):
-    print(f'{key} {value}')
+def fn1(t):
+    if t >= 1 and t <= 6 :
+       print("day")
+    else:
+       print('sorry there no available driver around you!')
+
+def fn2(t):
+    if t >= 7 and t <= 12 :
+       print("night")
+    else:
+       print('sorry there no available driver around you!!')
+
+def main(a):
+    fn1(a)
+    fn2(a)
+
+main(time)
+
