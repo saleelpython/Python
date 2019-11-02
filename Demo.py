c2321dfdd919c235ef5cf015faa859c539b803c0
SaleelBagde <saleel.python@gmail.com>
2019-11-02 07:27:06 +0530

    Commited on 02-11-2019

--- Demo.py
+++ Demo.py
@@ -5,6 +5,12 @@
 
 # Comprehensions in Python
 
+
+# Packing and Unpacking Arguments in Python
+# We use two operators * (for tuples) and ** (for dictionaries).
+
+
+
 # List Comprehensions
 
 # string='saleel'
@@ -529,7 +535,31 @@
 #     else:
 #         print(f'{x} has {store} char')
 
+#-----------------------------------------------------
 
+#DRY and WET codding
+
+
+# time = int(input("Enter Time "))
+
+# def fn1(t):
+#     if t >= 1 and t <= 6 :
+#        print("day")
+#     else:
+#        print('sorry there no available driver around you!')
+
+# def fn2(t):
+#     if t >= 7 and t <= 12 :
+#        print("night")
+#     else:
+#        print('sorry there no available driver around you!!
+#        ')
+
+# def main(a):
+#     fn1(a)
+#     fn2(a)
+
+# main(time)
 
 
 # -------------------------------
--- app.py
+++ app.py
@@ -1,20 +1,2 @@
-time = int(input("Enter Time "))
 
-def fn1(t):
-    if t >= 1 and t <= 6 :
-       print("day")
-    else:
-       print('sorry there no available driver around you!')
-
-def fn2(t):
-    if t >= 7 and t <= 12 :
-       print("night")
-    else:
-       print('sorry there no available driver around you!!')
-
-def main(a):
-    fn1(a)
-    fn2(a)
-
-main(time)
 
