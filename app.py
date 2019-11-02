42bd0c4a02200d599d3979bbe7ac0594577588c0
SaleelBagde <saleel.python@gmail.com>
2019-11-02 09:34:29 +0530

    Commited on 01-11-2019

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
+fruits = [('orange','banana','kiwi'),('banana','apple'),('blackberry','blueberry'), ('lime', 'kiwi', 'apple')]
 
+
+print(fruits[0])
+
+
+f = list()
+cnt=0
+def fn(l):
+   for i in range(len(l)):
+      if l[i][0] not in f:
+         f.append((l[i]))
+      
+fn(fruits)
+print(f)
 
