2f5c0bea51b5ebd0c3bb5fee86b830a21435e634
Saleel <saleel.python@gmail.com>
2019-11-09 11:25:29 +0530

    Commiter on 09-11-2019

--- Demo.py
+++ Demo.py
@@ -3,6 +3,13 @@
 
 #!C:\Users\saleel\AppData\Local\Programs\Python\Python37-32\python
 
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered and unindexed. No duplicate members.
+# Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+
+
 # Comprehensions in Python
 
 
