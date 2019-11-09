7cc58577d6bf9d0f27e9b3acfbb6c29719b25d6b
Saleel <saleel.python@gmail.com>
2019-11-09 11:18:20 +0530

    c

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-www = "www.google.com"
+# www = "www.google.com"
 
-d = dict()
+# d = dict()
 
-for i in range(len(www)):
-    d.setdefault(www[i], www.count(www[i]))
+# d['google'] = ["www.google.com","www.gmail.com","www.youtube.com"]
+# d["oracle"] = ["www.oracle.com", "www.mySQL.com","www.oracleNoSQL.com"]
 
-print(d)
 
+# for key, value in d.items():
+#     print(f'{key} {value}')
+#     print()
+
+
+numbers = (2,3,4,6,5,2,4,7,34,5,76,56,23,13,67,54,23,46,13,57,53,43,24,35,7)
+
+# def fn(n):
+#     return (n)
+
+x = map(lambda n :n if n > 50 else '', numbers)
+
+for key, value in enumerate(x):
+    print(f'{key} {value}')
