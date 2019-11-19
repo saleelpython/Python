a21ee67ed79a0112073c9ea5d4a3a32d5b422ed7
Saleel <saleel.python@gmail.com>
2019-11-19 11:13:56 +0530

    Commited on 19-11-2019

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+import pymongo
+person={"_id": 2, "name":"ram"}
+try:
+    mongoDBClient = pymongo.MongoClient(host='192.168.100.20', port=27017)
+    db = mongoDBClient['primaryDB']
+    print(f'Collection List in {db.name} {db.list_collection_names()}')
+    
+except pymongo.errors.DuplicateKeyError as ex:
+    print(f'pymongo.errors.DuplicateKeyError : {ex}')
 
+except pymongo.errors.CollectionInvalid as ex:
+    print(f'pymongo.errors.CollectionInvalid : {ex}')
 
-
-
-
+finally:
+    print()
+    print('-'  * 40 ,' ', sep="\n")
+    print("Document inserted!")
